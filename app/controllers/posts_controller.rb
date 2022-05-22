0e3d53e6f546e2691acd8ba20e808a454c945f26
Batuhan SONMEZER <batuhan.sonmezer@kellogg.northwestern.edu>
2022-05-22 07:17:44 +0000

    v2

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,17 @@
+class PostsController < ApplicationController
+
+
+    def index
+        @posts = Post.all 
+    end 
+    
+    def show
+        @post = Post.find_by({ "id" => params["id"] })
+    end
+
+    def new 
+        @post = Post.new
+    end
+
+
+end
